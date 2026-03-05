b9699d6abbf2db98c206a6f51e59ab198ac3c8e5
AsierMartinez2000 <asiermm2000@gmail.com>
2026-03-05 13:39:42 +0100

    MVC de la parte de Analisis

---
+++ app/models/analisisModel.php
@@ -0,0 +1,153 @@
+<?php
+require_once '../config/conexionBaseDatos.php'; // Asumiendo que tienes un archivo de conexión
+
+class analisisModel {
+    private $db;
+
+    public function __construct() {
+        $this->db =  ConexionBD::conexion(); // Método estático para obtener conexión
+    }
+
+    public function formSuenoCompletado($cod_participante){
+
+        try {
+            $sql = "SELECT cod_participante FROM sueno WHERE cod_participante = :cod_participante";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':cod_participante', $cod_participante, PDO::PARAM_INT);
+            $stmt->execute();
+            if($stmt->rowCount() > 0) {
+                return true; 
+            } else {
+                return false; 
+            }
+            
+        } catch (PDOException $e) {
+            error_log("Error al verificar participante: " . $e->getMessage());
+            return false;
+        }
+
+    }
+
+    public function formAntopoCompletado($cod_participante){
+        try {
+            $sql = "SELECT cod_participante FROM antropometrico WHERE cod_participante = :cod_participante";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':cod_participante', $cod_participante, PDO::PARAM_INT);
+            $stmt->execute();
+            if($stmt->rowCount() > 0) {
+                return true; 
+            } else {
+                return false; 
+            }
+            
+        } catch (PDOException $e) {
+            error_log("Error al verificar participante: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function formDietaCompletado($cod_participante){
+        try {
+            $sql = "SELECT cod_participante FROM alimentacion WHERE cod_participante = :cod_participante";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':cod_participante', $cod_participante, PDO::PARAM_INT);
+            $stmt->execute();
+            if($stmt->rowCount() > 0) {
+                return true; 
+            } else {
+                return false; 
+            }
+            
+        } catch (PDOException $e) {
+            error_log("Error al verificar participante: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function formFisicaCompletado($cod_participante){
+        try {
+            $sql = "SELECT cod_participante FROM actividad WHERE cod_participante = :cod_participante";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':cod_participante', $cod_participante, PDO::PARAM_INT);
+            $stmt->execute();
+            if($stmt->rowCount() > 0) {
+                return true; 
+            } else {
+                return false; 
+            }
+            
+        } catch (PDOException $e) {
+            error_log("Error al verificar participante: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function recogerDatosSueno($cod_participante){
+        try {
+            $sql = "SELECT * FROM sueno WHERE cod_participante = :cod_participante";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':cod_participante', $cod_participante, PDO::PARAM_INT);
+            
+            $stmt->execute();
+
+            $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+            
+        } catch (PDOException $e) {
+            error_log("Error al verificar participante: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function recogerDatosAntopo($cod_participante){
+        try {
+            $sql = "SELECT * FROM  antropometrico  WHERE cod_participante = :cod_participante";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':cod_participante', $cod_participante, PDO::PARAM_INT);
+            
+            $stmt->execute();
+
+            $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+            
+        } catch (PDOException $e) {
+            error_log("Error al verificar participante: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function recogerDatosDieta($cod_participante){
+        try {
+            $sql = "SELECT * FROM  alimentacion  WHERE cod_participante = :cod_participante";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':cod_participante', $cod_participante, PDO::PARAM_INT);
+            
+            $stmt->execute();
+
+            $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+            
+        } catch (PDOException $e) {
+            error_log("Error al verificar participante: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function recogerDatosFisica($cod_participante){
+        try {
+            $sql = "SELECT * FROM  actividad  WHERE cod_participante = :cod_participante";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':cod_participante', $cod_participante, PDO::PARAM_INT);
+            
+            $stmt->execute();
+
+            $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+            
+        } catch (PDOException $e) {
+            error_log("Error al verificar participante: " . $e->getMessage());
+            return false;
+        }
+    }
+}
+?>
---
+++ app/views/analisisView.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analisis</title>
+</head>
+<body>
+    <h1>Analisis de los formularios completados</h1>
+    
+
+<?php
+
+function imprimirAnalisisSueno($datosSueno){
+
+    echo "<table>";
+        foreach ($datosSueno as $fila){
+            echo "<tr>";
+            foreach ($fila as $celda){
+                echo "<td>".$celda."</td>";
+            }
+            echo "</tr>";
+        }
+    echo "</table>";
+}
+
+function imprimirAnalisisAntopo($datosAntopo){
+    echo "<table>";
+        foreach ($datosAntopo as $fila){
+            echo "<tr>";
+            foreach ($fila as $celda){
+                echo "<td>".$celda."</td>";
+            }
+            echo "</tr>";
+        }
+    echo "</table>";
+}
+
+function imprimirAnalisisDieta($datosDieta){
+    echo "<table>";
+        foreach ($datosDieta as $fila){
+            echo "<tr>";
+            foreach ($fila as $celda){
+                echo "<td>".$celda."</td>";
+            }
+            echo "</tr>";
+        }
+    echo "</table>";
+}
+
+function imprimirAnalisisFisica($datosFisica){
+    echo "<table>";
+        foreach ($datosFisica as $fila){
+            echo "<tr>";
+            foreach ($fila as $celda){
+                echo "<td>".$celda."</td>";
+            }
+            echo "</tr>";
+        }
+    echo "</table>";
+}
+
+
+?>
+
+</body>
+</html>
