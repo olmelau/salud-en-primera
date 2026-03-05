0a2f1e83bc1b538023660b03f1bfbaf4d5803d3e
olmelau <olmelau@gmail.com>
2026-03-05 18:48:34 +0100

    ya se puede añadir un participante a la base de datos

---
+++ app/views/nuevoParticipanteView.php
@@ -0,0 +1,32 @@
+<h1>Nuevo Participante</h1>
+
+ <?php if (isset($mensaje)): ?>
+        <div
+            style="color: <?php echo $tipo_mensaje == 'exito' ? 'green' : 'red'; ?>; padding: 10px; margin: 10px 0; border: 1px solid;">
+            <?php echo $mensaje; ?>
+        </div>
+    <?php endif; ?>
+
+<form action="index.php" method="post">
+    <input type="hidden" name="controller" value="nuevoParticipante">
+    <input type="hidden" name="action" value="nuevoParticipante">
+
+    <label for="nombre">Código Participante: </label>
+    <input type="number" name="cod_participante" id="cod_participante"><br><br>
+    <label for="centro_educativo">Centro Educativo</label>
+    <input type="text" name="centro_educativo" id="centro_educativo"><br><br>
+    <label for="familia_profesional">Familia profesional</label>
+    <input type="text" name="familia_profesional" id="familia_profesional"><br><br>
+    <label for="edad">Edad</label>
+    <input type="number" name="edad" id="edad"><br><br>
+    <select id="sexo" name="sexo" required>
+        <option value="">Selecciona una opción</option>
+        <option value="Mujer">Mujer</option>
+        <option value="Hombre">Hombre</option>
+        <option value="Prefiere no indicar">Prefiere no indicar</option>
+    </select>
+    <br><br>
+    <input type="submit" value="enviar">
+</form>
+
+
