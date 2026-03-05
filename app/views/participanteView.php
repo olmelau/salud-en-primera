580c182b2e92492672de27461891a5dcf0f5674a
AsierMartinez2000 <asiermm2000@gmail.com>
2026-03-05 19:51:20 +0100

    Soy una mierda que no sabe guardar

--- app/views/participanteView.php
+++ app/views/participanteView.php
@@ -3,8 +3,6 @@
 <p>Bienvenido al panel de participante</p>
 
 
-
-<!-- También puedes hacerlo con formulario POST -->
 <form method="POST" action="index.php?controller=login&action=logout">
     <button type="submit">Cerrar Sesión</button>
 </form>
